b3a067e841c1c7411789f5b22fc420f54bfc43ae
marianoefernandez <marianofernandez@MacBook-Air-de-Mariano.local>
2024-09-11 19:20:13 -0300

    Se agrego la funcion restar

--- funciones.py
+++ funciones.py
@@ -1,3 +1,7 @@
 def sumar(numero_uno:int,numero_dos:int) -> int:
     resultado = numero_uno + numero_dos
     return resultado
+
+def restar(numero_uno:int,numero_dos:int) -> int:
+    resultado = numero_uno - numero_dos
+    return resultado
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from funciones import *
 
 print(sumar(10,5))
+print(restar(10,5))
